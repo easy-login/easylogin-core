ffa9ffceccc426fe6367edf42e6ddaff0078420a
anhtn <anhtn1@five9.vn>
2018-08-14 17:22:13 +0700

    Update example

--- examples/site.py
+++ examples/site.py
@@ -6,11 +6,11 @@ app = Flask(__name__, template_folder='.')
 def auth_callback():
     token = request.args['token']
     provider = request.args['provider']
-    return render_template('index.html', provider=provider, token=token)
+    return render_template('demo.html', provider=provider, token=token)
 
 @app.route('/demo.html')
 def demo_page():
-    return render_template('index.html')
+    return render_template('demo.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
