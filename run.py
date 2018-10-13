8e8711075aa7186334d8f7c18537d87bbe679d41
tjeubaoit <anhtn.bk@gmail.com>
2018-10-13 08:43:25 +0700

    Allow config port to run at debug mode

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 from sociallogin import app
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    import sys
+    port = sys.argv[1] if len(sys.argv) > 1 else 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
