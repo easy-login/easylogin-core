f4122bcb5053e23cf0df8402b37b0b5e31bc1b2e
tjeubaoit <anhtn.bk@gmail.com>
2018-10-13 10:33:01 +0700

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export FLASK_APP=sociallogin:app
-export FLASK_ENV=development
+export FLASK_DEBUG=1
 
-flask run -h 0.0.0.0 -p "${1:-5000}"
+flask run -h 0.0.0.0 -p "${1:-5000}" --cert $2 --key $3
