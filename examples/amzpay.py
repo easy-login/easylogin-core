0fba7437ed29108855311d8a2be12d8bf57757ce
anhtn <anhtn1@five9.vn>
2018-12-24 17:15:17 +0700

    Update demo pay

--- examples/amzpay.py
+++ examples/amzpay.py
@@ -5,6 +5,7 @@ import time
 import random
 import logging
 import secrets
+import random
 
 from flask import Flask, Blueprint, render_template, abort, url_for, redirect, \
     request, flash, session, current_app as app
@@ -34,14 +35,44 @@ def settings():
     session['merchant_id'] = request.form['merchant-id']
     session['mws_access_key'] = request.form['mws-access-key']
     session['mws_secret_key'] = request.form['mws-secret-key']
-    session['client_id'] = request.form['client-id']
     session['order_reference_id'] = 'S01-9969307-1083016'
     return redirect('/amazon-pay/cart')
 
 
 @amazon_pay.route('/cart')
 def cart():
-    return render_template('cart.html')
+    items = [
+        {
+            'name': 'Easy Login Premium License 1 year',
+            'description': 'This license will allow you to access all features for premium accounts at '
+                           'easy-login.jp in 1 year.',
+            'image_url': 'https://cdn3.iconfinder.com/data/icons/essential-rounded/66/Rounded-22-48.png',
+            'price': random.randint(1, 5),
+            'quantity': random.randint(1, 3)
+        },
+        {
+            'name': 'Easy Login Premium License 3 years',
+            'description': 'This license will allow you to access all features for premium accounts at '
+                           'easy-login.jp in 3 years.',
+            'image_url': 'https://cdn3.iconfinder.com/data/icons/free-social-1/60/Star-48.png',
+            'price': random.randint(6, 10),
+            'quantity': random.randint(1, 3)
+        },
+        {
+            'name': 'Easy Login Premium License 5 years',
+            'description': 'This license will allow you to access all features for premium accounts at '
+                           'easy-login.jp in 5 years.',
+            'image_url': 'https://cdn4.iconfinder.com/data/icons/ios-web-user-interface-multi-circle-flat-vol-5/512/'
+                         'Crown_optimization_royal_princes_winner_premium_service-48.png',
+            'price': random.randint(11, 20),
+            'quantity': random.randint(1, 3)
+        }
+    ]
+    amount = 0
+    for item in items:
+        amount += item['price'] * item['quantity']
+    session['order_amount'] = str(amount * 1000)
+    return render_template('cart.html', items=items, total_amount=str(amount) + ',000')
 
 
 @amazon_pay.route('/set', methods=['GET'])
@@ -51,6 +82,41 @@ def set():
     else:
         session['amazon_Login_accessToken'] = request.cookies.get('amazon_Login_accessToken')
     return render_template('set.html')
+
+
+@amazon_pay.route('/review', methods=['POST'])
+def review():
+    client = AmazonPayClient(
+        mws_access_key=session['mws_access_key'],
+        mws_secret_key=session['mws_secret_key'],
+        merchant_id=session['merchant_id'],
+        sandbox=True,
+        region=REGION_CODE,
+        currency_code=CURRENCY_CODE,
+        log_enabled=True,
+        log_file_name="/tmp/amzpay.log",
+        log_level="DEBUG")
+
+    order_reference_id = request.form['orderReferenceId']
+    session['order_reference_id'] = order_reference_id
+
+    print('get details for orderId', session['order_reference_id'])
+
+    response = client.set_order_reference_details(
+        amazon_order_reference_id=order_reference_id,
+        order_total=session['order_amount'],
+        seller_note='My seller note.',
+        seller_order_id=secrets.token_hex(16),
+        store_name='My store name.',
+        custom_information='My custom information.')
+
+    if response.success:
+        response = client.get_order_reference_details(
+            amazon_order_reference_id=order_reference_id,
+            address_consent_token=session['amazon_Login_accessToken'])
+
+    order_detail = json.dumps(json.loads(response.to_json()), indent=4)
+    return render_template('review.html', order_detail=order_detail)
 
 
 @amazon_pay.route('/confirm', methods=['POST'])
@@ -79,7 +145,7 @@ def confirm():
         response = client.authorize(
             amazon_order_reference_id=session['order_reference_id'],
             authorization_reference_id=rand(),
-            authorization_amount='19.95',
+            authorization_amount=session['order_amount'],
             transaction_timeout=0,
             capture_now=False)
 
@@ -87,42 +153,6 @@ def confirm():
 
     return render_template(
         'confirm.html', confirm=pretty_confirm, authorize=pretty_authorize)
-
-
-@amazon_pay.route('/get_details', methods=['POST'])
-def get_details():
-    client = AmazonPayClient(
-        mws_access_key=session['mws_access_key'],
-        mws_secret_key=session['mws_secret_key'],
-        merchant_id=session['merchant_id'],
-        sandbox=True,
-        region=REGION_CODE,
-        currency_code=CURRENCY_CODE,
-        log_enabled=True,
-        log_file_name="/tmp/amzpay.log",
-        log_level="DEBUG")
-
-    order_reference_id = request.form['orderReferenceId']
-    session['order_reference_id'] = order_reference_id
-
-    print('get details for orderId', session['order_reference_id'])
-
-    response = client.set_order_reference_details(
-        amazon_order_reference_id=order_reference_id,
-        order_total='19.95',
-        seller_note='My seller note.',
-        seller_order_id=secrets.token_hex(16),
-        store_name='My store name.',
-        custom_information='My custom information.')
-
-    if response.success:
-        response = client.get_order_reference_details(
-            amazon_order_reference_id=order_reference_id,
-            address_consent_token=session['amazon_Login_accessToken'])
-
-    pretty = json.dumps(json.loads(response.to_json()), indent=4)
-
-    return pretty
 
 
 def rand():
