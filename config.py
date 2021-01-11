8cc7e9cbe14dfe6eeff185a05c4dd19dac5979e8
tjeubaoit <anhtn.bk@gmail.com>
2021-01-11 18:55:34 +0700

    Clean config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ load_dotenv()
 import os
 
 # Statement for enabling the development environment
-DEBUG = (os.getenv('DEBUG', 'True') != 'False')
+DEBUG = bool(os.getenv('DEBUG', 'True'))
 
 # Define the application directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -12,20 +12,32 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 TIME_ZONE = os.getenv('TIME_ZONE', 'Asia/Ho_Chi_Minh')
 
 # SERVER_NAME = 'http://localhost:5000'
-SECRET_KEY = os.getenv('SECRET_KEY', 'soci@ll0gin.c48COByeIVl0NKUxsfgYYw')
+if DEBUG:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'soci@ll0gin.c48COByeIVl0NKUxsfgYYw')
+else:
+    # You must provide SECRET_KEY in prodution
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+if not SECRET_KEY:
+    raise ValueError('SECRET_KEY must not be None')
 
 # Define the database
-SQLALCHEMY_DATABASE_URI = os.getenv(
-    'SQLALCHEMY_DATABASE_URI',
-    'mysql+pymysql://root:root@localhost/easylogin?charset=utf8mb4'
-)
+if DEBUG:
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'SQLALCHEMY_DATABASE_URI',
+        'mysql+pymysql://root:root@localhost/easylogin?charset=utf8mb4'
+    )
+else:
+    # You must provide SQLALCHEMY_DATABASE_URI in prodution
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_NATIVE_UNICODE = True
 
 DATABASE_CONNECT_OPTIONS = {}
 JSON_SORT_KEYS = True
 
-LOG_LEVEL = os.getenv('LOG_LEVEL', 'DEBUG')
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 LOG_FORMAT = os.getenv(
     'LOG_FORMAT',
     '[%(asctime)s] %(levelname)s %(filename)s:%(lineno)d - %(message)s'
